f3b8121a897d3f6a7408a53b86bc91daeb18a65b
eerovii <111981034+eerovii@users.noreply.github.com>
2022-10-27 13:19:29 +0300

    Update Moduuli9.2.py

--- Moduuli9.2.py
+++ Moduuli9.2.py
@@ -1,17 +1,19 @@
 class Auto:
-    def __init__(self, rekisteri, huiput):
-        self.rekkari = rekisteri
-        self.huippu = huiput
+    def __init__(self, rekisteritunnus, huippunopeudet):
+        self.rekkari = rekisteritunnus
+        self.huippu = huippunopeudet
         self.nopeus = 0
         self.kuljettu = 0
+        #auto def
 
     def kiihdytä(self, muutos):
         self.nopeus += muutos
         if self.nopeus > self.huippu: self.nopeus = self.huippu
         if self.nopeus < 0: self.nopeus = 0
-
+        #kiihdytä def
     def __str__(self):
         return f'{self.rekkari} \n {self.huippu} \n {self.nopeus} \n {self.kuljettu}'
+        #def string
 
 auto = Auto('ABC-123', 142)
 
